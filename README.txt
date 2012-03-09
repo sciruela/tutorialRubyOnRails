b19e5209ede7df534689eda3f4354bcb4779602b
Sergio Ciruela <Sergio Ciruela>
2012-03-09 01:21:33 +0100

    first commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+I have followed the next tutorial about Ruby on Rails (RoR) to learn how it works.
+
+http://guides.rubyonrails.org/getting_started.html#getting-up-and-running-quickly-with-scaffolding
---
+++ example1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Example1::Application.config.secret_token = 'e7c99f44bdf5b2408005269f9b4a545c660f1bb977a0dae450844ae18d48566660fe68f8bcc98690bedfa40910532e52fba61dae09a4e30f4db4c5db2648517c'
